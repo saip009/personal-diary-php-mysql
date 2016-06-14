304cf87184ab51255263929d7d0dbfca8bab286c
Praneeth <saip009@gmail.com>
2016-06-14 16:52:42 +0530

    added Sign-In option

--- index.php
+++ index.php
@@ -2,47 +2,82 @@
 	
 	session_start();
 
-	if (isset($_POST['submit'])) {
+	$link = mysqli_connect("localhost","root","password","diaryDB");
 
-		$error = "";
+	if (isset($_POST['submit'])){
 
-		if (!$_POST['email']) {
-			$error .= "<br />Please enter your e-mail";
-		}
-		elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$error .= "<br />Please enter a valid e-mail address";
-		}
+		if ($_POST['submit'] == "Sign Up") {
 
-		if (!$_POST['password']) {
-			$error .= "<br />Please enter your password";
-		}
-		else {
-			if (strlen($_POST['password'])<8) {
-				$error .= "<br />Please enter a password with at lease 8 letters.";
-				mysqli_query($link, $query);
-				echo "You've been signed up!";
+			$error = "";
+
+			if (!$_POST['email']) {
+				$error .= "<br />Please enter your e-mail";
 			}
+			elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+				$error .= "<br />Please enter a valid e-mail address";
+			}
+
+			if (!$_POST['password']) {
+				$error .= "<br />Please enter your password";
+			}
+			else {
+				if (strlen($_POST['password'])<8) {
+					$error .= "<br />Please enter a password with at lease 8 letters.";
+				}
+			}
+
+			if ($error) echo "There were following error(s) in your Sign-Up details:".$error;
+			else {
+
+				$query = "SELECT * FROM user WHERE email='".mysqli_real_escape_string($link, $_POST['email'])."'";
+				$result = mysqli_query($link, $query);
+				// echo $results = mysqli_num_rows($result);
+				$results = mysqli_num_rows($result);
+
+				if ($results)	echo "This email is already registered. Do you want to Sign-In?";
+				else {
+					$query = "INSERT INTO user (email,password) VALUES('".mysqli_real_escape_string($link, $_POST['email'])."' , '". md5(md5($_POST['email']).$_POST['password'])."')";
+					if (mysqli_query($link, $query))	echo "You have successfully Signed-Up!";
+					// else echo "Error: " . $query . "<br>" . mysqli_error($link);
+
+					$_SESSION['id'] = mysqli_insert_id($link);
+
+					// print_r($_SESSION);
+
+					// redirect to logged in page.
+
+				}
+
+			}
+
 		}
 
-		if ($error) echo "There were following error(s) in your Sign-Up details:".$error;
-		else {
+		if ($_POST['submit'] == "Sign In") {
 
-			$link = mysqli_connect("localhost","root","password","diaryDB");
-			$query = "SELECT * FROM user WHERE email='".mysqli_real_escape_string($link, $_POST['email'])."'";
-			$result = mysqli_query($link, $query);
-			// echo $results = mysqli_num_rows($result);
-			$results = mysqli_num_rows($result);
+			$error = "";
 
-			if ($results)	echo "This email is already registered. Do you want to Sign-In?";
+			if (!$_POST['signinemail']) {
+				$error .= "<br />Please enter your e-mail";
+			}
+			elseif (!filter_var($_POST['signinemail'], FILTER_VALIDATE_EMAIL)) {
+				$error .= "<br />Please enter a valid e-mail address";
+			}
+
+			if (!$_POST['signinpassword']) {
+				$error .= "<br />Please enter your password";
+			}
+
+			if ($error) echo "There were following error(s) in your Sign-In details:".$error;
 			else {
-				$query = "INSERT INTO user (email,password) VALUES('".mysqli_real_escape_string($link, $_POST['email'])."' , '". md5(md5($_POST['email']).$_POST['password'])."')";
-				if (mysqli_query($link, $query))	echo "You have successfully Signed-Up!";
-				// else echo "Error: " . $query . "<br>" . mysqli_error($link);
 
-				$_SESSION['id'] = mysqli_insert_id($link);
+				$query = "SELECT  * FROM user WHERE email='".mysqli_real_escape_string($link, $_POST['signinemail'])."' AND password = '".md5(md5($_POST['signinemail']).$_POST['signinpassword'])."' LIMIT 1";
+				// echo $query;
+				$result = mysqli_query($link, $query);
+				$row = mysqli_fetch_array($result);
 
-				print_r($_SESSION);
-
+				if ($row) {
+					$_SESSION['id'] = $row['id'];
+				}
 			}
 
 		}
@@ -53,8 +88,17 @@
 
 <form method="POST" novalidate>
 
-	<input type="email" name="email" id="email" />
-	<input type="password" name="password" id="password" />
+	<input type="email" name="email" id="email" value="<?php echo isset($_POST['email']) ? addslashes($_POST['email']) : ''; ?>" />
+	<input type="password" name="password" id="password" value="<?php echo isset($_POST['password']) ? addslashes($_POST['password']) : ''; ?>" />
 	<input type="submit" name="submit" value="Sign Up" />
+
+</form>
+
+
+<form method="POST" novalidate>
+
+	<input type="email" name="signinemail" id="signinemail" value="<?php echo isset($_POST['email']) ? addslashes($_POST['email']) : ''; ?>" />
+	<input type="password" name="signinpassword" id="signinpassword" value="<?php echo isset($_POST['password']) ? addslashes($_POST['password']) : ''; ?>" />
+	<input type="submit" name="submit" value="Sign In" />
 
 </form>
